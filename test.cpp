75f3c1b982d13ed25abcadb9dd4fb55c438ae5a0
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-05-19 18:39:50 -0300

    test file of a simple connection

--- test.cpp
+++ test.cpp
@@ -1,71 +1,111 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <iostream>
-#include <cstring>
-#include <cstdio>
+#include <iostream>     // std::cout, std::endl
+#include <cstring>      // std::memset
+#include <cstdio>       // std::perror
 
+#include <sys/types.h>  // socklen_t
+#include <sys/socket.h> // socket(), bind(), listen(), accept()
+#include <netinet/in.h> // sockaddr_in, htons(), INADDR_ANY
+#include <unistd.h>     // read(), close()
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    char buffer[1024];
-    int port = 6667; // Default IRC port
 
-    // Create the server socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
+    // --------- Setup sockets ----------------
+
+    // structs representing an IPv4 socket address
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+
+    // data type to store size of socket structures
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    // buffer to store data read from the client
+    char buffer[1024];
+
+    // default IRC port
+    int port = 6667;
+
+    // --------- Create the server socket ----------------
+
+    // creating a socket and returns a fd
+    // socket(address family for IPv4, TCP socket, default protocol)
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { // error check
+        std::perror("socket");
         return 1;
     }
 
-    // Set up the server address struct
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET; // Set the address family to IPv4
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all available interfaces
-    server_addr.sin_port = htons(port); // Convert the port to network byte order
+    // --------- Setup server address struct ----------------
 
-    // Bind the socket to the address and port
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
+    // clears the struct to avoid garbage values
+    std::memset(&server_addr, 0, sizeof(server_addr));
+
+    // AF_INET = using IPv4
+    server_addr.sin_family = AF_INET;
+    // INADDR_ANY = listen on all networks
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    // htons(port) = conversion from host byte order to network byte order
+    server_addr.sin_port = htons(port);
+
+
+    // Allow address reuse (prevents "Address already in use" error)
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
         close(server_fd);
         return 1;
     }
 
-    // Listen for incoming connections
+    // --------- Bind the socket ----------------
+    
+    // bind links the socket to the network (IP address and port)
+    // bind(socket fd, socket struct, size of socket struct)
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::perror("bind");
+        close(server_fd); // error check
+        return 1;
+    }
+
+    // --------- Listen incoming connections ----------------
+    
+    // listen makes the socket a listening socket
+    // listen(socket fd, max connections that can wait in line)
     if (listen(server_fd, 5) < 0) {
-        perror("listen");
+        std::perror("listen"); // error check
         close(server_fd);
         return 1;
     }
 
     std::cout << "Server listening on port " << port << std::endl;
 
-    // Accept a connection
-    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (client_fd < 0) {
-        perror("accept");
+    // --------- Accept client's connection ----------------
+
+    // accept(socket fd, socket struct new fd, socket struct size)
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_fd < 0) { // error check
+        std::perror("accept");
         close(server_fd);
         return 1;
     }
 
     std::cout << "Client connected" << std::endl;
 
-    // Read data from the client
+    // --------- Read data from the client ----------------
+
+    // read(client socket, buffer to store data received, size of buffer)
     ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-    if (bytes_read < 0) {
-        perror("read");
+    if (bytes_read < 0) { // error check
+        std::perror("read");
+        // Close sockets
         close(client_fd);
         close(server_fd);
         return 1;
     }
-
     buffer[bytes_read] = '\0'; // Null-terminate the buffer
+   
+    // print message received and stored in buffer
     std::cout << "Received message: " << buffer << std::endl;
 
-    // Close the client and server sockets
+    // Close sockets
     close(client_fd);
     close(server_fd);
 
