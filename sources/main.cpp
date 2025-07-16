8578ac858a670442233f1be575af3dc2bcf46309
Sabrina Ferreira Nascimento <sabrifer@student.42sp.org.br>
2025-07-16 14:59:07 -0300

    refactor/ removed parse function from main and added to utils folder in cpp in hpp; added setupSignals function to utils folders

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,32 +1,5 @@
 #include "../includes/server/Server.hpp"
-
-#include <sstream>
-#include <utility>
-
-std::pair<int, std::string> validatePortAndPassword(char **av) {
-
-    // validate port from 1024 to 65535
-    int port;
-    std::stringstream ss(av[1]);
-    if (!(ss >> port) || !(ss >> std::ws).eof()) {
-        throw std::invalid_argument("Error! Invalid port number: '" + std::string(av[1]) + "'");
-    }
-    if (port < 1024 || port > 65535) {
-        throw std::invalid_argument("Error! Port number is out of range of available ports: '" + std::string(av[1]) + "'");
-    }
-
-    // validate password
-    std::string password = av[2];
-
-    if (password.find(" ") != std::string::npos || (password.length() < 8 || password.length() > 16)) {
-        throw std::invalid_argument("Error: Password must be 8-16 characters long and contain no spaces.");
-    }
-
-    std::pair<int, std::string> pair;
-    pair.first = port;
-    pair.second = password;
-    return pair;
-}
+#include "../includes/utils/Utils.hpp"
 
 int main(int ac, char **av) {
 
@@ -37,19 +10,9 @@ int main(int ac, char **av) {
     }
     Server server;
     try {
-        // ctrl+c
-        signal(SIGINT, Server::signalHandler);
-        // ctrl+'\'
-        signal(SIGQUIT, Server::signalHandler);
-        // kill [pid]
-        signal(SIGTERM, Server::signalHandler);
-        //Server server;
-        std::pair<int, std::string> pair = validatePortAndPassword(av);
-        server.setPortNumber(pair.first);
-        server.setServerPassword(pair.second);
-        std::cout << "Server starting on port " << server.getPortNumber()
-              << " with password '" << server.getServerPassword() << "'" << std::endl;
-
+        setupSignals(Server::signalHandler);
+        std::pair<int, std::string> pair = parsePortAndPassword(av);
+        server = Server(pair.first, pair.second);
         server.initServer();
     } catch (const std::exception& e) {
         std::cerr << "Error! " << e.what() << std::endl;
